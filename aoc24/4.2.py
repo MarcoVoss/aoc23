824dc8327f049c2cea993e13ef48317d94aa99a3
MarcoVoss <33320412+MarcoVoss@users.noreply.github.com>
2024-12-05 21:27:35 +0100

    Update 4.2.py

--- aoc24/4.2.py
+++ aoc24/4.2.py
@@ -1,28 +1,15 @@
 
 M = [list(m.strip()) for m in open("4.2.txt", 'r').readlines()]
-R = ["X", "M", "A", "S"]
 
-
-def check(position: tuple[int, int]) -> int:
-    y, x = position
-    return (
-        (0 < y < len(M) - 1) and
-        (0 < x < len(M[0]) - 1) and
-        (
-            (M[y+1][x+1] == "M" and M[y-1][x-1] == "S") or
-            (M[y-1][x-1] == "M" and M[y+1][x+1] == "S")
-        ) and
-        (
-            (M[y+1][x-1] == "M" and M[y-1][x+1] == "S") or
-            (M[y-1][x+1] == "M" and M[y+1][x-1] == "S")
-        )
+print(sum(
+    1
+    for i, m in enumerate(M)
+    for j, n in enumerate(m)
+    if (
+        (n == "A") and
+        (0 < i < len(M) - 1) and
+        (0 < j < len(M[0]) - 1) and
+        ("MS" in [f"{M[i+1][j+1]}{M[i-1][j-1]}", f"{M[i-1][j-1]}{M[i+1][j+1]}"]) and
+        ("MS" in [f"{M[i+1][j-1]}{M[i-1][j+1]}", f"{M[i-1][j+1]}{M[i+1][j-1]}"])
     )
-
-
-count = 0
-for i, m in enumerate(M):
-    for j, n in enumerate(m):
-        if n == "A" and check((i, j)):
-            count += 1
-
-print(count)
+))
