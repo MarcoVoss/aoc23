f0cc6024f056d8aff268d8082c0e1e30fd06f8dc
MarcoVoss <33320412+MarcoVoss@users.noreply.github.com>
2025-12-04 22:04:32 +0100

    add 4.1.25

---
+++ aoc25/4.1.py
@@ -0,0 +1,24 @@
+result = 0
+
+M = []
+
+for positions in open("4.1.txt", 'r').readlines():
+    M.append(list(positions.strip()))
+
+for x, row in enumerate(M):
+    for y, position in enumerate(row):
+        if position == "@":
+            paper = 0
+            for dx in [1, 0, -1]:
+                for dy in [1, 0, -1]:
+                    if (
+                        (dx != 0 or dy != 0) and
+                        (0 <= x + dx < len(M)) and
+                        (0 <= y + dy < len(row)) and
+                        (M[x+dx][y+dy] == "@")
+                    ):
+                        paper += 1
+            if paper < 4:
+                result += 1
+
+print(result)
